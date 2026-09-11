d81febd9276e95870a9430f86fb01248bc973f6e
JhonatanJG2 <jhonatan162704stiven@gmail.com>
2026-09-11 13:00:01 -0500

    Agregar middleware CORS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', userRoutes);
 
